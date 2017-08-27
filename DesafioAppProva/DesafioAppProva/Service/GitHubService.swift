736c831d67f710de67828d77b365f39c5d8f9173
Diogo Ribeiro de Oliveira <diogoro.dba@gmail.com>
2017-08-27 16:59:59 -0300

    Criação da classe que irá trabalhar com as chamadas a api do git hub

--- DesafioAppProva/DesafioAppProva/Service/GitHubService.swift
+++ DesafioAppProva/DesafioAppProva/Service/GitHubService.swift
@@ -7,3 +7,55 @@
 //
 
 import Foundation
+import Alamofire
+import SwiftyJSON
+
+let kUrlBase = "https://api.github.com/"
+
+class GitHubService {
+    
+    // "https://api.github.com/search/repositories?q=language:Java&sort=stars&page=1" Url de exemplo para o primeiro metodo
+    class func getRepositores(page: String, completion: @escaping (_ data: SearchResult?, _ error: NSError?)-> ()) {
+        Alamofire.request(kUrlBase + "search/repositories?q=language:Java&sort=stars&page=\(page)", method: .get, parameters: nil, encoding: URLEncoding.default, headers: nil).responseJSON { (dataResponse) in
+            
+            guard let data = dataResponse.data, dataResponse.response?.statusCode == 200 else {
+                if dataResponse.response?.statusCode == 204 {
+                    completion(nil,nil)
+                } else {
+                    let error = dataResponse.error as NSError?
+                    completion(nil, error)
+                }
+                return
+            }
+            
+            let results = SearchResult(json: JSON(data))
+            completion(results, nil)
+        }
+        
+    }
+    
+    // "https://api.github.com/repos/ReactiveX/RxJava/pulls" Url de exemplo para o segundo metodo
+    class func getPullsRequest(repository:String, completion: @escaping (_ pulls: [PullRequest]?, _ error: NSError?)->()) {
+        Alamofire.request(kUrlBase + "repos/\(repository)/pulls", method: .get, parameters: nil, encoding: URLEncoding.default, headers: nil).responseJSON { (dataResponse) in
+            guard let data = dataResponse.data, dataResponse.response?.statusCode == 200 else {
+                if dataResponse.response?.statusCode == 204 {
+                    completion(nil,nil)
+                } else {
+                    let error = dataResponse.error as NSError?
+                    completion(nil, error)
+                }
+                return
+            }
+            
+            var pulls: [PullRequest] = []
+            let array = JSON(data).array
+            if let arrayAux = array {
+                for element in arrayAux {
+                    let pull = PullRequest(json: element)
+                    pulls.append(pull)
+                }
+            }
+            completion(pulls, nil)
+        }
+    }
+}
