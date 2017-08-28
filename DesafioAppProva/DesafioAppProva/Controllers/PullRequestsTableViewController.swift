0d7d661fff9cbac026523f32ede0bc5f72cad359
Diogo Ribeiro de Oliveira <diogoro.dba@gmail.com>
2017-08-28 11:48:20 -0300

    Adição da chamada ao serviço do github, configuração das informações da tableview e implementação para abrir a url do pull request.

--- DesafioAppProva/DesafioAppProva/Controllers/PullRequestsTableViewController.swift
+++ DesafioAppProva/DesafioAppProva/Controllers/PullRequestsTableViewController.swift
@@ -11,15 +11,29 @@ import UIKit
 class PullRequestsTableViewController: UITableViewController {
 
     var repositoryName:String!
+    var pullRequestsList: [PullRequest] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        self.navigationItem.title = repositoryName
+        GitHubService.getPullsRequest(repository: repositoryName) { (pulls, error) in
+            guard let pullsList = pulls else {
+                if let err = error {
+                    var message = "Erro inesperado ao acessar a api do GitHub"
+                    if err.code == 990 {
+                        message = String(describing: err.userInfo["MESSAGE_API_ERROR"])
+                    } else if err.code == 999 {
+                        message = String(describing: err.userInfo["ERROR_INVALID_PARSING"])
+                    }
+                    let alert = Alert(title: "Erro", message: message)
+                    self.present(alert.getAlert(), animated: true, completion: nil)
+                }
+                return
+            }
+            self.pullRequestsList.append(contentsOf: pullsList)
+            self.tableView.reloadData()
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,67 +45,37 @@ class PullRequestsTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return self.pullRequestsList.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "pullCell", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "pullCell", for: indexPath) as! PullRequestTableViewCell
+        let pull = self.pullRequestsList[indexPath.row]
+        
+        cell.setValuesCell(pullRequest: pull)
+        
         return cell
     }
 
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let pullSelect = self.pullRequestsList[indexPath.row]
+        guard let pullHtmlUrl = URL(string: pullSelect.htmlUrl ?? "") else {
+            let alert = Alert(title: "Erro", message: "Pull Request não possui url para exibição no browser")
+            self.present(alert.getAlert(), animated: true, completion: nil)
+            return
+        }
+        if UIApplication.shared.canOpenURL(pullHtmlUrl) {
+            if #available(iOS 10.0, *) {
+                UIApplication.shared.open(pullHtmlUrl, options: [:], completionHandler: nil)
+            } else {
+                UIApplication.shared.openURL(pullHtmlUrl)
+            }
+        }
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
