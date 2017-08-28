197e28298a723e1b071aabd7c7c417862e72f72f
Diogo Ribeiro de Oliveira <diogoro.dba@gmail.com>
2017-08-28 11:36:28 -0300

    Classe para criar Alertas

---
+++ DesafioAppProva/DesafioAppProva/Util/Alerta.swift
@@ -0,0 +1,25 @@
+//
+//  Alerta.swift
+//  DesafioAppProva
+//
+//  Created by Diogo Ribeiro de Oliveira on 28/08/17.
+//  Copyright © 2017 Diogo Ribeiro de Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class Alert {
+    
+    let alert = UIAlertController()
+    
+    init(title:String, message:String) {
+        alert.title = title
+        alert.message = message
+        let doneButton = UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: nil)
+        alert.addAction(doneButton)
+    }
+    
+    func getAlert() -> UIAlertController {
+        return alert
+    }
+}
