b043197f40639ade43aa0c83872218653019873c
Diogo Ribeiro de Oliveira <diogoro.dba@gmail.com>
2017-08-28 03:35:35 -0300

    Commit inicial da classe, já contendo a configuração base para fazer a primeira chamada e passar a informação necessária para a próxima tela.

--- DesafioAppProva/DesafioAppProva/Controllers/SearchResultsTableViewController.swift
+++ DesafioAppProva/DesafioAppProva/Controllers/SearchResultsTableViewController.swift
@@ -10,14 +10,13 @@ import UIKit
 
 class SearchResultsTableViewController: UITableViewController {
 
+    var page: Int = 1
+    var repositoryList: [Repository] = []
+    var isFinishLoad = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        loadItens()
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,67 +28,55 @@ class SearchResultsTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return self.repositoryList.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "repositoryCell", for: indexPath) as! RepositoryTableViewCell
 
-        // Configure the cell...
+        let repository = self.repositoryList[indexPath.row]
+        cell.setValuesCell(repository: repository)
 
         return cell
     }
-    */
+    
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let repositoryName = self.repositoryList[indexPath.row].fullName
+        performSegue(withIdentifier: "pullSegue", sender: repositoryName)
     }
-    */
 
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    func loadItens() {
+        if !isFinishLoad {
+            GitHubService.getRepositores(page: "\(self.page)") { (result, error) in
+                guard let result = result else {
+                    self.isFinishLoad = true
+                    // mandar mensagem de aviso
+                    return
+                }
+                self.repositoryList.append(contentsOf: result.repositores)
+                self.page = self.page + 1
+                self.tableView.reloadData()
+            }
+        }
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "pullSegue", let repositoryName = sender as? String {
+            let destination = segue.destination as! PullRequestsTableViewController
+            destination.repositoryName = repositoryName
+        }
+        
     }
-    */
 
 }
