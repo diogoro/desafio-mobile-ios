99246415b53b3d0418d1d87d801846c39f8f04a4
Diogo Ribeiro de Oliveira <diogoro.dba@gmail.com>
2017-08-28 03:28:23 -0300

    Commit inicial das classes.

---
+++ DesafioAppProva/DesafioAppProva/Cells/PullRequestTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  PullRequestTableViewCell.swift
+//  DesafioAppProva
+//
+//  Created by Diogo Ribeiro de Oliveira on 28/08/17.
+//  Copyright © 2017 Diogo Ribeiro de Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class PullRequestTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    
+
+}
