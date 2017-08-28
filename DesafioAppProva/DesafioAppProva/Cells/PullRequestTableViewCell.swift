2ff18a8b0ff5a7ce558fcc28688689a44f758464
Diogo Ribeiro de Oliveira <diogoro.dba@gmail.com>
2017-08-28 11:37:50 -0300

    Implementação das atribuição de valores para cada elemento da célula.

--- DesafioAppProva/DesafioAppProva/Cells/PullRequestTableViewCell.swift
+++ DesafioAppProva/DesafioAppProva/Cells/PullRequestTableViewCell.swift
@@ -7,14 +7,33 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class PullRequestTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var decriptionP: UILabel!
+    @IBOutlet weak var username: UILabel!
+    @IBOutlet weak var date: UILabel!
+    @IBOutlet weak var ownerImage: UIImageView!
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
+    func setValuesCell(pullRequest: PullRequest) {
+        self.title.text = pullRequest.title
+        self.decriptionP.text = pullRequest.body
+        self.username.text = pullRequest.user?.login
+        self.date.text = pullRequest.createdAt
+        guard let urlImage = URL(string: pullRequest.user?.avatarUrl ?? "") else {
+            return
+        }
+        
+        ownerImage.af_setImage(withURL: urlImage, placeholderImage: nil, filter: CircleFilter(), progress: nil, progressQueue: DispatchQueue.main, imageTransition: UIImageView.ImageTransition.crossDissolve(1.0), runImageTransitionIfCached: true, completion: nil)
+    }
     
 
 }
